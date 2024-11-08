b0c64474f097c828ee90e13233dee025089610db
Silver0se13 <gurleens0013@gmail.com>
2024-11-07 17:56:08 -0700

    Lab 2 completed

---
+++ animals.js
@@ -0,0 +1,24 @@
+bear.addEventListener('click', function(){
+    console.log("This is a bear");
+    document.getElementById("animal").src="images/bear.webp";
+})
+
+elk.addEventListener('click', function(){
+    console.log("This is an elk");
+    document.getElementById("animal").src="images/elk.webp";
+})
+
+moose.addEventListener('click', function(){
+    console.log("This is a moose");
+    document.getElementById("animal").src="images/moose.webp";
+})
+
+sheep.addEventListener('click', function(){
+    console.log("This is a sheep");
+    document.getElementById("animal").src="images/sheep.webp";
+})
+
+wolf.addEventListener('click', function(){
+    console.log("This is a wolf");
+    document.getElementById("animal").src="images/wolf.webp";
+})
