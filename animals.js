b5a51ea39670517acd22ff5e9b9ab6c485219ed6
Silver0se13 <gurleens0013@gmail.com>
2024-11-07 18:10:48 -0700

    Fixed Case sensitive items
    changed "images/sheep.webp" to "Images/sheep.webp"

--- animals.js
+++ animals.js
@@ -1,24 +1,24 @@
 bear.addEventListener('click', function(){
     console.log("This is a bear");
-    document.getElementById("animal").src="images/bear.webp";
+    document.getElementById("animal").src="Images/bear.webp";
 })
 
 elk.addEventListener('click', function(){
     console.log("This is an elk");
-    document.getElementById("animal").src="images/elk.webp";
+    document.getElementById("animal").src="Images/elk.webp";
 })
 
 moose.addEventListener('click', function(){
     console.log("This is a moose");
-    document.getElementById("animal").src="images/moose.webp";
+    document.getElementById("animal").src="Images/moose.webp";
 })
 
 sheep.addEventListener('click', function(){
     console.log("This is a sheep");
-    document.getElementById("animal").src="images/sheep.webp";
+    document.getElementById("animal").src="Images/sheep.webp";
 })
 
 wolf.addEventListener('click', function(){
     console.log("This is a wolf");
-    document.getElementById("animal").src="images/wolf.webp";
+    document.getElementById("animal").src="Images/wolf.webp";
 })
